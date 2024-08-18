5072dc14d2576ffff43af91a2eb861679530650a
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-18 16:06:40 +0430

    Add a get route for the getSingleProduct details

--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { getProducts, newProduct } from "../controllers/productControllers.js";
+import {
+  getProducts,
+  newProduct,
+  getProductDetails,
+} from "../controllers/productControllers.js";
 
 const router = express.Router();
 
 router.route("/products").get(getProducts);
+router.route("/products/:id").get(getProductDetails);
 router.route("/admin/products").post(newProduct);
 
 export default router;
