7a89117a7ccbd9a8d33b09704a7eaf3bfb7d5235
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-18 16:16:20 +0430

    Create the updateProduct controller action

--- api/controllers/productControllers.js
+++ api/controllers/productControllers.js
@@ -29,3 +29,20 @@ export const getProductDetails = async (req, res) => {
     product,
   });
 };
+
+// Update product details => /api/v1/products/:id
+export const updateProduct = async (req, res) => {
+  let product = await Product.findById(req.params?.id);
+  if (!product) {
+    return res.status(404).json({
+      error: "Product not found",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json({
+    product,
+  });
+};
