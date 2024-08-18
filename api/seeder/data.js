1cba1ae91782d6769b4a77ce5638a6294c51fa8f
hafiz1379 <hafizrasa1379@gmail.com>
2024-08-18 14:09:28 +0430

    Add seeder script, endpoint, and controller for product creation

---
+++ api/seeder/data.js
@@ -0,0 +1,221 @@
+export default [
+    {
+      name: "SanDisk Ultra 128GB SDXC UHS-I Memory Card up to 80MB/s",
+      price: 45.89,
+      description:
+        "Ultra-fast cards (2) to take better pictures and Full HD videos (1) with your compact to mid-range point-and-shoot cameras and camcorders. With SanDisk Ultra SDXC UHS-I cards you’ll benefit from faster downloads, high capacity, and better performance to capture and store 128GB (5) of high quality pictures and Full HD video (1). Take advantage of ultra-fast read speeds of up to 80MB/s (3) to save time moving photos and videos from the card to your computer. From a leader in flash memory storage, SanDisk Ultra SDXC UHS-I cards are compatible with SDHC and SDXC digital devices, and come with a 10-year limited warranty (6).",
+      ratings: 4.5,
+      images: [
+        {
+          public_id: "shopit/demo/nkkjkta63uiazppzkmjf",
+          url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577488/shopit/demo/nkkjkta63uiazppzkmjf.jpg",
+        },
+        {
+          public_id: "shopit/demo/hz1iwdqzrvxtfxincvju",
+          url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577488/shopit/demo/hz1iwdqzrvxtfxincvju.jpg",
+        },
+        {
+          public_id: "shopit/demo/oc2dvvkxyxukc13y9pjv",
+          url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577488/shopit/demo/oc2dvvkxyxukc13y9pjv.jpg",
+        },
+      ],
+      category: "Electronics",
+      seller: "Ebay",
+      stock: 50,
+      numOfReviews: 32,
+      reviews: [],
+    },
+    {
+      name: "CAN USB FD Adapter (GC-CAN-USB-FD)",
+      price: 315,
+      description:
+        "Monitor a CAN network, write a CAN program and communicate with industrial, medical, automotive or other CAN based device. Connect CAN FD and CAN networks to a computer via USB with the CAN USB FD adapter.",
+      ratings: 1.65,
+      images: [
+        {
+          public_id: "shopit/demo/e3hweb3tbp5zacfi564c",
+          url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577509/shopit/demo/e3hweb3tbp5zacfi564c.jpg",
+        },
+        {
+          public_id: "shopit/demo/mjcc0kg0lzxegrauh8qc",
+          url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577509/shopit/demo/mjcc0kg0lzxegrauh8qc.jpg",
+        },
+        {
+          public_id: "shopit/demo/iruzmpgrhb6xyqheppxg",
+          url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577509/shopit/demo/iruzmpgrhb6xyqheppxg.jpg",
+        },
+        {
+          public_id: "shopit/demo/bj3auijqcxxvkwdohznb",
+          url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577510/shopit/demo/bj3auijqcxxvkwdohznb.jpg",
+        },
+      ],
+      category: "Electronics",
+      seller: "Amazon",
+      stock: 0,
+      numOfReviews: 2,
+      reviews: [],
+    },
+    {
+      name: "CHARMOUNT Full Motion TV Wall Mount Swivel",
+      price: 26.99,
+      description:
+        "CHARMOUNT TV MOUNT UNIVERSAL DESIGN - Has your TV been received? Tilted TV wall mount is for 26 - 55 TVs weight up to 88lbs 40 kg. Our tilt TV mount has a compatible faceplate that fits VESA 75X75mm (3x3). CHARMOUNT TV MOUNT UNIVERSAL DESIGN - Has your TV been received? Tilted TV wall mount is for 26 - 55 TVs weight up to 88lbs 40 kg. Our tilt TV mount has a compatible faceplate that fits VESA 75X75mm (3x3) CHARMOUNT TV MOUNT UNIVERSAL DESIGN - Has your TV been received? Tilted TV wall mount is for 26 - 55 TVs weight up to 88lbs 40 kg. Our tilt TV mount has a compatible faceplate that fits VESA 75X75mm (3x3).",
+      ratings: 3.5,
+      images: [
+        {
+          public_id: "shopit/demo/yqqwxrgst2pi3frtngaw",
+          url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577531/shopit/demo/yqqwxrgst2pi3frtngaw.jpg",
+        },
+        {
+          public_id: "shopit/demo/pgcwny8xyshsnwaiikbm",
+          url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577531/shopit/demo/pgcwny8xyshsnwaiikbm.jpg",
+        },
+      ],
+      category: "Electronics",
+      seller: "Amazon",
+      stock: 1,
+      numOfReviews: 12,
+      reviews: [],
+    },
+    {
+      name: "Bose QuietComfort 35 II Wireless Bluetooth Headphones",
+      price: 299,
+      description:
+        "What happens when you clear away the noisy distractions of the world? Concentration goes to the next level. You get deeper into your music, your work, or whatever you want to focus on. That’s the power of Bose QuietComfort 35 wireless headphones II. Put them on and get closer to what you’re most passionate about. And that’s just the beginning. QuietComfort 35 wireless headphones II are now enabled with Bose AR",
+      ratings: 4.5,
+      images: [
+        {
+          public_id: "shopit/demo/yxbrklp7snr7dgxfrxu9",
+          url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577543/shopit/demo/yxbrklp7snr7dgxfrxu9.jpg",
+        },
+        {
+          public_id: "shopit/demo/i8ge1ilin2fz9tk2okni",
+          url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577543/shopit/demo/i8ge1ilin2fz9tk2okni.jpg",
+        },
+      ],
+      category: "Headphones",
+      seller: "Amazon",
+      stock: 11,
+      numOfReviews: 112,
+      reviews: [],
+    },
+    {
+      name: "Apple AirPods with Charging Case (Wired)",
+      price: 126.99,
+      description:
+        "AirPods with Charging Case: More than 24 hours listening time, up to 18 hours talk time; AirPods (single charge): Up to 5 hours listening time, up to 3 hours talk time or 15 minutes in the case equals up to 3 hours listening time or up to 2 hours talk time.",
+      ratings: 4.8,
+      images: [
+        {
+          public_id: "shopit/demo/yj00oalanbzmbzctsbln",
+          url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577557/shopit/demo/yj00oalanbzmbzctsbln.jpg",
+        },
+        {
+          public_id: "shopit/demo/mijzcdd5sle8a2any1i8",
+          url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577557/shopit/demo/mijzcdd5sle8a2any1i8.jpg",
+        },
+        {
+          public_id: "shopit/demo/kxckb0o7fbdwqgmugoct",
+          url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577557/shopit/demo/kxckb0o7fbdwqgmugoct.jpg",
+        },
+      ],
+      category: "Electronics",
+      seller: "Amazon",
+      stock: 122,
+      numOfReviews: 2671,
+      reviews: [],
+    },
+    {
+      name: "Cable Boom Microphone - Volume Control for Playstation PS4 or Xbox",
+      price: 27.99,
+      description:
+        "DESIGN INFO - 3.5mm male to 2.5mm male audio cable adapter with Upgraded Flexible, Detachable Boom Mic which also enables rotary Volume Control and Mute Switch. SteelFlex Arm for perfect microphone positioning. INPUT COMPATIBILITY - Devices supporting 3.5mm audio output such as gaming PS4 / Xbox One controller, PC, Laptop, iPhone and Android Phone.",
+      ratings: 4.1,
+      images: [
+        {
+          public_id: "shopit/demo/xmndneguqtrcutpf9wcz",
+          url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577572/shopit/demo/xmndneguqtrcutpf9wcz.jpg",
+        },
+      ],
+      category: "Accessories",
+      seller: "Amazon",
+      stock: 1123,
+      numOfReviews: 6,
+      reviews: [],
+    },
+    {
+      name: "Nikon D3500 W/ AF-P DX NIKKOR 18-55mm f/3.5-5.6G VR Black",
+      price: 496.95,
+      description:
+        "You don't need to be a photographer to know a great photo when you see one. And you don't need to be a photographer to take a great photo—you just need the D3500. It's as easy to use as a point-and-shoot, but it takes beautiful DSLR photos and videos that get noticed. It feels outstanding in your hands, sturdy and balanced with controls where you want them. It's compact, durable and versatile, ideal for travel. And it works seamlessly with compatible smartphones, making it easier than ever to share your great photos. Even if you've never picked up a DSLR camera, you can take beautiful pictures with D3500.",
+      ratings: 4.7,
+      images: [
+        {
+          public_id: "shopit/demo/q6ybe84qlkzwet9qwptz",
+          url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577585/shopit/demo/q6ybe84qlkzwet9qwptz.jpg",
+        },
+        {
+          public_id: "shopit/demo/niqqio1faynnscubkkmd",
+          url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577585/shopit/demo/niqqio1faynnscubkkmd.jpg",
+        },
+        {
+          public_id: "shopit/demo/dy3nhjgsdgqcnrwf8u2h",
+          url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577585/shopit/demo/dy3nhjgsdgqcnrwf8u2h.jpg",
+        },
+      ],
+      category: "Cameras",
+      seller: "Amazon",
+      stock: 131,
+      numOfReviews: 222,
+      reviews: [],
+    },
+    {
+      name: "Apple MacBook Air (13-inch, 8GB RAM, 256GB SSD Storage) - Space Gray",
+      price: 949.99,
+      description:
+        "Stunning 13.3-inch Retina display with True Tone technology, Backlit Magic Keyboard and Touch ID, Tenth-generation, Intel Core i3 processor, Intel Iris Plus Graphics, Fast SSD storage, 8GB of memory, Stereo speakers with wider stereo sound",
+      ratings: 4.9,
+      images: [
+        {
+          public_id: "shopit/demo/jzqaj98nnhy0hcsilx9y",
+          url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577597/shopit/demo/jzqaj98nnhy0hcsilx9y.jpg",
+        },
+        {
+          public_id: "shopit/demo/welkq4dgfi5267usmj0n",
+          url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577597/shopit/demo/welkq4dgfi5267usmj0n.jpg",
+        },
+        {
+          public_id: "shopit/demo/pabtjloyzenmr6z8klcr",
+          url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577600/shopit/demo/pabtjloyzenmr6z8klcr.jpg",
+        },
+      ],
+      category: "Laptops",
+      seller: "Amazon",
+      stock: 0,
+      numOfReviews: 56,
+      reviews: [],
+    },
+    {
+      name: "Kauffman Orchards Homegrown McIntosh Apples",
+      price: 2.75,
+      description:
+        "McIntosh is a pretty red/green apple with white flesh Homegrown in Kauffman's orchard in healthy Lancaster County soil McIntosh apples are famous for snacking, pies, salads, and applesauce Protected with soft, high-density foam for safe shipping to your door Not for sale to California, Oregon, or Washington due to state laws governing fresh produce",
+      ratings: 5,
+      images: [
+        {
+          public_id: "shopit/demo/mq0zpyzmzqokphs9m9we",
+          url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577619/shopit/demo/mq0zpyzmzqokphs9m9we.jpg",
+        },
+        {
+          public_id: "shopit/demo/pkyy8rifxpesiwpy23kk",
+          url: "http://res.cloudinary.com/udemy-courses/image/upload/v1698577619/shopit/demo/pkyy8rifxpesiwpy23kk.jpg",
+        },
+      ],
+      category: "Food",
+      seller: "Kauffman's Fruit Farm & Market",
+      stock: 500,
+      numOfReviews: 1,
+      reviews: [],
+    },
+  ];
+  
