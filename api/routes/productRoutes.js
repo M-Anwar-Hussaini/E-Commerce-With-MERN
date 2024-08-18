19e80ea936b3f6dab5c2ccff22bfc59c1a1badf0
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-18 16:26:31 +0430

    Chain the the delete product route to the product routes.

--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -4,12 +4,18 @@ import {
   newProduct,
   getProductDetails,
   updateProduct,
+  deleteProduct,
 } from "../controllers/productControllers.js";
 
 const router = express.Router();
 
 router.route("/products").get(getProducts);
-router.route("/products/:id").get(getProductDetails).put(updateProduct);
+router
+  .route("/products/:id")
+  .get(getProductDetails)
+  .put(updateProduct)
+  .delete(deleteProduct);
+
 router.route("/admin/products").post(newProduct);
 
 export default router;
