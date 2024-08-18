88c868c724d082660762c8ceabc1496848e178ab
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-18 16:16:41 +0430

    Add a put route to update a product.

--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -3,12 +3,13 @@ import {
   getProducts,
   newProduct,
   getProductDetails,
+  updateProduct,
 } from "../controllers/productControllers.js";
 
 const router = express.Router();
 
 router.route("/products").get(getProducts);
-router.route("/products/:id").get(getProductDetails);
+router.route("/products/:id").get(getProductDetails).put(updateProduct);
 router.route("/admin/products").post(newProduct);
 
 export default router;
