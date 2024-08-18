5d34728dd25fe3c800680f433ffe4f32ea6db22d
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-18 11:37:37 +0430

    Create a new route for products.

---
+++ api/routes/productRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { getProducts } from "../controllers/productControllers.js";
+
+const router = express.Router();
+
+router.route("/products").get(getProducts);
+
+export default router;
