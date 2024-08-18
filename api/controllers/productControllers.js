e9ce84d0102bc256705e05b892a6383ef3008f82
hafiz1379 <hafizrasa1379@gmail.com>
2024-08-18 13:36:58 +0430

    Add newProduct controller for creating products

--- api/controllers/productControllers.js
+++ api/controllers/productControllers.js
@@ -1,5 +1,17 @@
+import Product from "../models/productModel.js";
+
+// Create new product => /api/v1/products
 export const getProducts = (req, res) => {
   res.status(200).json({
     message: "All products",
   });
 };
+
+// Cretae new product => /api/v1/admin/products
+export const newProduct = async (req, res) => {
+  const product = await Product.create(req.body);
+
+  res.status(201).json({
+    product,
+  });
+}
