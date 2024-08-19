49778b8989c2cfe8964cc42a20b3018ebed4e775
hafiz1379 <hafizrasa1379@gmail.com>
2024-08-19 15:47:41 +0430

    Add error handling middleware for API

---
+++ api/middlewares/errors.js
@@ -0,0 +1,21 @@
+import process from "process";
+
+export default (err, req, res, next) => {
+  let error = {
+    statusCode: err.statusCode || 500,
+    message: err.message || "Internal Server Error",
+  };
+
+  if (process.env.NODE_ENV === "DEVELOPMENT") {
+    res.status(error.statusCode).json({
+      message: error.message,
+      error: err,
+      stack: err?.stack,
+    });
+  }
+  if (process.env.NODE_ENV === "PRODUCTION") {
+    res.status(error.statusCode).json({
+      message: error.message,
+    });
+  }
+};
