9a7abfd81451375a3625bd0dfd7e55f9d6f6cd94
hafiz1379 <hafizrasa1379@gmail.com>
2024-08-19 15:47:30 +0430

    Add errorHandler utility class

---
+++ api/utils/errorHandler.js
@@ -0,0 +1,11 @@
+class ErrorHandlere extends Error {
+    constructor(message, statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+
+        // Capture the stack trace
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+export default ErrorHandlere;
