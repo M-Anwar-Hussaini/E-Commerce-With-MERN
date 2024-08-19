6a9bbfb08622547ea1e6947d70e2a98a3d7c0d12
hafiz1379 <hafizrasa1379@gmail.com>
2024-08-19 15:48:09 +0430

    Add error handling for getProductDetails controller

--- api/controllers/productControllers.js
+++ api/controllers/productControllers.js
@@ -1,4 +1,5 @@
 import Product from "../models/productModel.js";
+import ErrorHandler from "../utils/errorHandler.js";
 
 // Create new product => /api/v1/products
 export const getProducts = async (req, res) => {
@@ -18,12 +19,10 @@ export const newProduct = async (req, res) => {
 };
 
 // Get single product details => /api/v1/products/:id
-export const getProductDetails = async (req, res) => {
+export const getProductDetails = async (req, res, next) => {
   const product = await Product.findById(req.params?.id);
   if (!product) {
-    return res.status(404).json({
-      error: "Product not found",
-    });
+    return next(new ErrorHandler("Product not found", 404));
   }
   res.status(200).json({
     product,
