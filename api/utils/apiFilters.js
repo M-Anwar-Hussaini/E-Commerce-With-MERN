7fdeba902ff18195539961316612a216bddde448
hafiz1379 <hafizrasa1379@gmail.com>
2024-08-20 09:09:20 +0430

    Refactor APIFilters class for improved query filtering

--- api/utils/apiFilters.js
+++ api/utils/apiFilters.js
@@ -17,6 +17,24 @@ class APIFilters {
     this.query = this.query.find({ ...keyword });
     return this;
   }
+
+  filters() {
+    const queryCopy = { ...this.queryString };
+
+    // Removing fields from the query
+    const fieldsToRemove = ["keyword"];
+    fieldsToRemove.forEach((el) => delete queryCopy[el]);
+
+    // Advance filter for price, ratings etc
+    let queryString = JSON.stringify(queryCopy);
+    queryString = queryString.replace(
+      /\b(gt|gte|lt|lte)\b/g,
+      (match) => `$${match}`
+    );
+
+    this.query = this.query.find(JSON.parse(queryString));
+    return this;
+  }
 }
 
 export default APIFilters;
