85d78535423f1fc456b582a8e55d662857f4189d
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-21 21:18:31 +0430

    Register the login controller in the /api/v1/login route

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,7 +1,8 @@
-import express from 'express';
-import { registerUser } from '../controllers/authControllers.js';
+import express from "express";
+import { loginUser, registerUser } from "../controllers/authControllers.js";
 const router = express.Router();
 
-router.route('/register').post(registerUser);
+router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
 
 export default router;
