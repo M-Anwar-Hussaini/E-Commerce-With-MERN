6201884a29224c3d8757f0039992aa9cc528316b
hafiz1379 <hafizrasa1379@gmail.com>
2024-08-21 10:21:27 +0430

    Add registerUser controller for user registration

---
+++ api/controllers/authControllers.js
@@ -0,0 +1,16 @@
+import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
+import User from "../models/userModel.js";
+
+export const registerUser = catchAsyncErrors(async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  res.status(201).json({
+    success: true,
+  });
+});
