989df7da2b16827f8b2bfb0f772f496c0f127c43
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-21 21:17:32 +0430

    Iplement the login controller

--- api/controllers/authControllers.js
+++ api/controllers/authControllers.js
@@ -1,5 +1,25 @@
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
 import User from "../models/userModel.js";
+import ErrorHandler from "../utils/errorHandler.js";
+
+export const loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(new ErrorHandler("Please enter and password.", 400));
+  }
+
+  // Search for user
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or password", 400));
+  }
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    return next(new ErrorHandler("Invalid email or password", 400));
+  }
+  const token = user.getJwtToken();
+  return res.status(200).json({ token });
+});
 
 export const registerUser = catchAsyncErrors(async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -10,7 +30,7 @@ export const registerUser = catchAsyncErrors(async (req, res, next) => {
     password,
   });
 
-  const token = user.getJwtToken()
+  const token = user.getJwtToken();
 
   res.status(201).json({
     token,
