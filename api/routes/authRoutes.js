4db80918cc4f47b8c175e64915536058d922ca55
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-23 09:35:30 +0430

    Register the logout controller to the authRoutes

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/authControllers.js";
+import {
+  loginUser,
+  logout,
+  registerUser,
+} from "../controllers/authControllers.js";
 const router = express.Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+router.route("/logout").post(logout);
 
 export default router;
