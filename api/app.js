6da25613fd5c78b7e637f87e94ca0f4fe893cd96
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-23 09:12:32 +0430

    Register the cookie-parser package in the app.js

--- api/app.js
+++ api/app.js
@@ -1,30 +1,28 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import process from 'process';
-import productRoutes from './routes/productRoutes.js';
-import connectToDatabase from './config/dbConnect.js';
-import errorMiddleware from './middlewares/errors.js';
-import authRoutes from './routes/authRoutes.js';
+import express from "express";
+import dotenv from "dotenv";
+import process from "process";
+import productRoutes from "./routes/productRoutes.js";
+import connectToDatabase from "./config/dbConnect.js";
+import errorMiddleware from "./middlewares/errors.js";
+import authRoutes from "./routes/authRoutes.js";
+import cookieParser from "cookie-parser";
 
 // Hanlde the uncaught exception errors
-process.on('uncaughtException', (err) => {
+process.on("uncaughtException", (err) => {
   console.log(`ERROR: ${err}`);
-  console.log('Shutting down the server due to uncaught exception');
+  console.log("Shutting down the server due to uncaught exception");
   process.exit(1);
 });
 
 // Intial configurations
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
 const app = express();
-
-// console.log(undefinedVariable);  => Test the uncaughtException error handler
-
 app.use(express.json());
-
+app.use(cookieParser());
 
 // Defining the routes:
-app.use('/api/v1', productRoutes);
-app.use('/api/v1', authRoutes);
+app.use("/api/v1", productRoutes);
+app.use("/api/v1", authRoutes);
 
 // Middleware to handle errors
 app.use(errorMiddleware);
@@ -38,10 +36,10 @@ const server = app.listen(process.env.PORT, () => {
 });
 
 // Handle Unhandled Promise Rejection Error
-process.on('unhandledRejection', (err) => {
+process.on("unhandledRejection", (err) => {
   console.log(`ERROR: ${err}`);
   console.log(
-    'Shutting down the server due to the Unhandled Promise Rejection',
+    "Shutting down the server due to the Unhandled Promise Rejection",
   );
   server.close(() => {
     process.exit(1);
