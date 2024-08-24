af3fa37d7581513a1e7005a7f8e7e2f92a31d69e
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-24 23:27:23 +0430

    Handle the duplicate email error

--- api/middlewares/errors.js
+++ api/middlewares/errors.js
@@ -1,20 +1,20 @@
-import process from 'process';
-import ErrorHandler from '../utils/errorHandler.js';
+import process from "process";
+import ErrorHandler from "../utils/errorHandler.js";
 
 export default (err, req, res, next) => {
   let error = {
     statusCode: err?.statusCode || 500,
-    message: err?.message || 'Internal Server Error',
+    message: err?.message || "Internal Server Error",
   };
 
   // Handle invalid mongoose ID
-  if (err?.name === 'CastError') {
+  if (err?.name === "CastError") {
     const message = `Resource not found. Invalid ${err?.path}`;
     error = new ErrorHandler(message, 404);
   }
 
   // Handle validation errors
-  if (err?.name === 'ValidationError') {
+  if (err?.name === "ValidationError") {
     const message = Object.values(err?.errors).map((errObj) => errObj?.message);
     error = new ErrorHandler(message, 400);
 
@@ -26,7 +26,25 @@ export default (err, req, res, next) => {
       */
   }
 
-  if (process.env.NODE_ENV === 'DEVELOPMENT') {
+  // Handle mongoose duplicate key error
+  if (err?.code === 11000) {
+    const message = `Duplicate ${Object.keys(err?.keyValue)} entered`;
+    error = new ErrorHandler(message, 400);
+  }
+
+  // Hanlde wrong jwt error
+  if (err?.name === "JsonWebTokenError") {
+    const message = `JSON Web Token is invalid. Try again!!!`;
+    error = new ErrorHandler(message, 400);
+  }
+
+  // Hanlde expired jwt error
+  if (err?.name === "TokenExpiredError") {
+    const message = `JSON Web Token is expired. Try again!!!`;
+    error = new ErrorHandler(message, 400);
+  }
+
+  if (process.env.NODE_ENV === "DEVELOPMENT") {
     // Handle the errors in the development environment
     return res.status(error.statusCode).json({
       message: error.message,
@@ -35,7 +53,7 @@ export default (err, req, res, next) => {
     });
   }
   // Handle the errors in the production environment
-  if (process.env.NODE_ENV === 'PRODUCTION') {
+  if (process.env.NODE_ENV === "PRODUCTION") {
     return res.status(error.statusCode).json({
       message: error.message,
     });
