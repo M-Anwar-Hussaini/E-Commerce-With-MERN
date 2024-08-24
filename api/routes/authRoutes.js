eea5f4491bade1d22dc3d183326b04c3231e2eb3
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-24 23:15:23 +0430

    register a route for reseting the password

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -4,6 +4,7 @@ import {
   loginUser,
   logout,
   registerUser,
+  resetPassword,
 } from "../controllers/authControllers.js";
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").post(logout);
 router.route("/password/forget").post(forgetPassword);
+router.route("/password/reset/:token").put(resetPassword);
 
 export default router;
