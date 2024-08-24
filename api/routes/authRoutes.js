409ba3c175557fd1883535c5fdc47dcb9d4c1fdc
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-24 22:39:43 +0430

    Add a route for the forget password

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+  forgetPassword,
   loginUser,
   logout,
   registerUser,
@@ -9,5 +10,6 @@ const router = express.Router();
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").post(logout);
+router.route("/password/forget").post(forgetPassword);
 
 export default router;
