770bed30ffbcd3376e397c6b97a4551350cc4b93
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-25 09:36:12 +0430

    Register the all uses and single user route to the authRoute

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,6 +1,8 @@
 import express from "express";
 import {
   forgetPassword,
+  getAllUsers,
+  getUserDetails,
   getUserProfile,
   loginUser,
   logout,
@@ -9,16 +11,25 @@ import {
   updatePassword,
   updateProfile,
 } from "../controllers/authControllers.js";
-import { isAuthenticatedUser } from "../middlewares/auth.js";
+import { authorizeRoles, isAuthenticatedUser } from "../middlewares/auth.js";
 const router = express.Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").post(logout);
+
 router.route("/password/forget").post(forgetPassword);
 router.route("/password/reset/:token").put(resetPassword);
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
+
 router.route("/me").get(isAuthenticatedUser, getUserProfile);
 router.route("/me/update").put(isAuthenticatedUser, updateProfile);
-router.route("/password/update").put(isAuthenticatedUser, updatePassword);
+
+router
+  .route("/admin/users")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllUsers);
+router
+  .route("/admin/users/:id")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getUserDetails);
 
 export default router;
