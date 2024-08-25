b017a7da3eb0acdfec0834397629b7fc60e80ed1
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-25 08:49:12 +0430

    Register the current logged-in user.

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import {
   forgetPassword,
+  getUserProfile,
   loginUser,
   logout,
   registerUser,
   resetPassword,
 } from "../controllers/authControllers.js";
+import { isAuthenticatedUser } from "../middlewares/auth.js";
 const router = express.Router();
 
 router.route("/register").post(registerUser);
@@ -13,5 +15,6 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(logout);
 router.route("/password/forget").post(forgetPassword);
 router.route("/password/reset/:token").put(resetPassword);
+router.route("/me").get(isAuthenticatedUser, getUserProfile);
 
 export default router;
