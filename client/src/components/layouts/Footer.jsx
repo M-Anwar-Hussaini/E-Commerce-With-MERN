9506f8b5776022393789a4ed772dee4c6a79340a
hafiz1379 <hafizrasa1379@gmail.com>
2024-08-27 08:53:24 +0430

    Update copyright year in footer component

--- client/src/components/layouts/Footer.jsx
+++ client/src/components/layouts/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="py-1 pt-5">
       <p className="text-center mt-1 fw-bold">
-        ShopIT - 2020-2023, All Rights Reserved
+        ShopIT - 2020-2024, All Rights Reserved
       </p>
     </footer>
   );
