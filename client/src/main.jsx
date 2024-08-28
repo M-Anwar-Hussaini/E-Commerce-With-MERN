41c856e03355a1d93874373961280094c30f897b
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-28 12:09:01 +0430

    Create the productItem component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,9 +6,9 @@ import "./index.css";
 import { store } from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Provider store={store}>
+  <Provider store={store}>
+    <StrictMode>
       <App />
-    </Provider>
-  </StrictMode>,
+    </StrictMode>
+  </Provider>,
 );
