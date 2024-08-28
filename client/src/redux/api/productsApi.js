9b0fc25ff61f85d9118322b6739fecfca2dff2f1
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-28 10:53:03 +0430

    Create productApi using the redux RTK

---
+++ client/src/redux/api/productsApi.js
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const productApi = createApi({
+  reducerPath: "productApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "/api/v1" }),
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: (params) => ({
+        url: "/products",
+      }),
+    }),
+  }),
+});
+
+export const { useGetProductsQuery } = productApi;
