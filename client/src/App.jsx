ef29e0eb67d757e9120ea9aaed1867b2b1bcfdce
hafiz1379 <hafizrasa1379@gmail.com>
2024-08-28 13:15:20 +0430

    Add react-hot-toast for displaying notifications

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/layouts/Footer";
 import Header from "./components/layouts/Header";
 import Home from "./components/Home";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <Router>
     <div className="App">
+      <Toaster position="top-center" />
       <Header />
       <div className="container">
         <Routes>
