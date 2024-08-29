0db31f3074ae350d675f92053e5115cc8651861f
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-08-29 18:27:05 +0430

    Add min and max searchParms to the productApi

--- client/src/redux/api/productsApi.js
+++ client/src/redux/api/productsApi.js
@@ -10,6 +10,8 @@ export const productApi = createApi({
         params: {
           page: params?.page,
           keyword: params?.keyword,
+          "price[gte]": params.min,
+          "price[lte]": params.max,
         },
       }),
     }),
