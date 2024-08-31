6b5e5020eb895019e2e2de2f3603f1d29338824e
hafiz1379 <hafizrasa1379@gmail.com>
2024-08-31 17:04:50 +0430

    Add authApi to handle authentication requests and integrate with store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { productApi } from "./api/productsApi";
+import { authApi } from "./api/authApi";
 
 export const store = configureStore({
   reducer: {
     [productApi.reducerPath]: productApi.reducer,
+    [authApi.reducerPath]: authApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(productApi.middleware),
+    getDefaultMiddleware().concat(productApi.middleware, authApi.middleware),
 });
