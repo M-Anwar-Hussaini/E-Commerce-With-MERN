b2df47c166777bd7ad4f8349db4f3c9c6a9dfa4a
hafiz1379 <hafizrasa1379@gmail.com>
2024-08-31 17:24:00 +0430

    Remove unnecessary import in Login component

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useLoginMutation } from "../../redux/api/authApi";
 import toast from "react-hot-toast";
 
