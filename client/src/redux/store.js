682265bcf13050a3130b9bb9d810d9cd63cae90e
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-09-01 07:50:03 +0430

    Register the userApi and userSlice to the store.

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { productApi } from "./api/productsApi";
-import { authApi } from "./api/authApi";
+
+import userReducer from "./features/userSlice";
+
+import productApi from "./api/productsApi";
+import authApi from "./api/authApi";
+import userApi from "./api/userApi";
 
 export const store = configureStore({
   reducer: {
+    auth: userReducer,
     [productApi.reducerPath]: productApi.reducer,
     [authApi.reducerPath]: authApi.reducer,
+    [userApi.reducerPath]: userApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(productApi.middleware, authApi.middleware),
+    getDefaultMiddleware().concat(
+      productApi.middleware,
+      authApi.middleware,
+      userApi.middleware,
+    ),
 });
