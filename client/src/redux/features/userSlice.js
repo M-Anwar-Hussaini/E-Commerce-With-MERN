8faa536b0480c754f629ce637f6895ade2fd64a1
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-09-01 07:48:01 +0430

    Create userSlice using react-redux

---
+++ client/src/redux/features/userSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+const userSlice = createSlice({
+  name: "userSlice",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
+    setIsAuthenticated: (state, action) => {
+      state.isAuthenticated = action.payload;
+    },
+  },
+});
+
+export default userSlice.reducer;
+export const { setIsAuthenticated, setUser } = userSlice.actions;
