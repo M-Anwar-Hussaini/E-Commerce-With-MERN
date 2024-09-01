6e18d36d86b1ae16e7833c0b59d9fa8d09d2c53e
hafiz1379 <hafizrasa1379@gmail.com>
2024-09-01 11:18:17 +0430

    Add UpdateProfile component for user profile update functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import ProductDetails from "./components/product/ProductDetails";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Profile from "./components/user/Profile";
+import UpdateProfile from "./components/user/UpdateProfile";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/me/profile" element={<Profile />} />
+        <Route path="/me/update_profile" element={<UpdateProfile />} />
         </Routes>
       </div>
       <Footer />
