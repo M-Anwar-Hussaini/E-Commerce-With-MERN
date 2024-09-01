9073f0eb5b84f291e8dabdedf1601d88f61491de
hafiz1379 <hafizrasa1379@gmail.com>
2024-09-01 10:57:34 +0430

    Add Profile component for user profile page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import { Toaster } from "react-hot-toast";
 import ProductDetails from "./components/product/ProductDetails";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Profile from "./components/user/Profile";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="/products/:id" element={<ProductDetails />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/me/profile" element={<Profile />} />
         </Routes>
       </div>
       <Footer />
