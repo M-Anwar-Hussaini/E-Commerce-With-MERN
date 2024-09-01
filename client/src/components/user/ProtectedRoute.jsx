4f33bd2642f999a49f46ef0cd9e37ed2a0126ccd
hafiz1379 <hafizrasa1379@gmail.com>
2024-09-01 12:13:41 +0430

    Add ProtectedRoute component for authenticated user routes

---
+++ client/src/components/user/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import Loader from "../layouts/Loader";
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated, loading } = useSelector((state) => state.auth);
+
+  if (loading) return <Loader />;
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
