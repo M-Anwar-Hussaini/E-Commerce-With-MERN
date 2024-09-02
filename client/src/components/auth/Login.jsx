0a79f4d6a678e76456b64a466da62f8dda9450c9
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-09-02 16:48:17 +0430

    Add title to the Login component

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -4,6 +4,7 @@ import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Loader from "../layouts/Loader";
+import MetaData from "../layouts/MetaData";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -36,59 +37,62 @@ const Login = () => {
   }
 
   return (
-    <div className="row wrapper">
-      <div className="col-10 col-lg-5">
-        <form className="shadow rounded bg-body" onSubmit={submitHandler}>
-          <h2 className="mb-4">Login</h2>
-          <div className="mb-3">
-            <label htmlFor="email_field" className="form-label">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email_field"
-              className="form-control"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
+    <>
+      <MetaData title="Login" />
+      <div className="row wrapper">
+        <div className="col-10 col-lg-5">
+          <form className="shadow rounded bg-body" onSubmit={submitHandler}>
+            <h2 className="mb-4">Login</h2>
+            <div className="mb-3">
+              <label htmlFor="email_field" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email_field"
+                className="form-control"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
 
-          <div className="mb-3">
-            <label htmlFor="password_field" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password_field"
-              className="form-control"
-              name="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
+            <div className="mb-3">
+              <label htmlFor="password_field" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password_field"
+                className="form-control"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
 
-          <a href="/password/forgot" className="float-end mb-4">
-            Forgot Password?
-          </a>
-
-          <button
-            id="login_button"
-            type="submit"
-            className="btn w-100 py-2"
-            disabled={isLoading}
-          >
-            {isLoading ? "Logging in..." : "LOGIN"}
-          </button>
-
-          <div className="my-3">
-            <a href="/register" className="float-end">
-              New User?
+            <a href="/password/forgot" className="float-end mb-4">
+              Forgot Password?
             </a>
-          </div>
-        </form>
+
+            <button
+              id="login_button"
+              type="submit"
+              className="btn w-100 py-2"
+              disabled={isLoading}
+            >
+              {isLoading ? "Logging in..." : "LOGIN"}
+            </button>
+
+            <div className="my-3">
+              <a href="/register" className="float-end">
+                New User?
+              </a>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
