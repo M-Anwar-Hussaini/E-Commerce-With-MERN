75fe889565f2088fc84192614cac631a250e49ff
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-09-02 15:37:46 +0430

    Register the cartSlice to the store.

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -5,10 +5,12 @@ import userReducer from "./features/userSlice";
 import productApi from "./api/productsApi";
 import authApi from "./api/authApi";
 import userApi from "./api/userApi";
+import cartReducer from "./features/cartSlice";
 
 export const store = configureStore({
   reducer: {
     auth: userReducer,
+    carts: cartReducer,
     [productApi.reducerPath]: productApi.reducer,
     [authApi.reducerPath]: authApi.reducer,
     [userApi.reducerPath]: userApi.reducer,
