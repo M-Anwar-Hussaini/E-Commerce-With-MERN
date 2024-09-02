71b5d62a8afdb2f32999651ae8c48597a10cb433
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-09-02 15:36:14 +0430

    Remove the RestrictMode from the Application

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
@@ -7,8 +6,6 @@ import { store } from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <StrictMode>
-      <App />
-    </StrictMode>
+    <App />
   </Provider>,
 );
