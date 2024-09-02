ba8babcb184a5991ec3086307e62dd677fa6e6c8
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-09-02 16:51:43 +0430

    Add title to the Product Details

--- client/src/components/product/ProductDetails.jsx
+++ client/src/components/product/ProductDetails.jsx
@@ -6,6 +6,7 @@ import Loader from "../layouts/Loader";
 import StarRatings from "react-star-ratings";
 import { useDispatch } from "react-redux";
 import { setCartItem } from "../../redux/features/cartSlice";
+import MetaData from "../layouts/MetaData";
 
 const ProductDetails = () => {
   const [activeImg, setActiveImg] = useState("");
@@ -51,120 +52,125 @@ const ProductDetails = () => {
   if (isLoading) return <Loader />;
 
   return (
-    <div>
-      <div className="row d-flex justify-content-around">
-        <div className="col-12 col-lg-5 img-fluid" id="product_image">
-          <div className="p-3">
-            <img
-              className="d-block w-100"
-              src={activeImg}
-              alt={product?.name}
-              width="340"
-              height="390"
-            />
+    <>
+      <MetaData title="Product Details" />
+      <div>
+        <div className="row d-flex justify-content-around">
+          <div className="col-12 col-lg-5 img-fluid" id="product_image">
+            <div className="p-3">
+              <img
+                className="d-block w-100"
+                src={activeImg}
+                alt={product?.name}
+                width="340"
+                height="390"
+              />
+            </div>
+            <div className="row justify-content-start mt-5">
+              {product?.images?.map((img) => (
+                <div key={img.url} className="col-2 ms-4 mt-2">
+                  <a role="button">
+                    <img
+                      className={`d-block border rounded p-3 cursor-pointer ${
+                        img.url === activeImg ? "border-warning" : ""
+                      }`}
+                      height="100"
+                      width="100"
+                      src={img?.url}
+                      alt={img?.url}
+                      onClick={() => setActiveImg(img.url)}
+                    />
+                  </a>
+                </div>
+              ))}
+            </div>
           </div>
-          <div className="row justify-content-start mt-5">
-            {product?.images?.map((img) => (
-              <div key={img.url} className="col-2 ms-4 mt-2">
-                <a role="button">
-                  <img
-                    className={`d-block border rounded p-3 cursor-pointer ${
-                      img.url === activeImg ? "border-warning" : ""
-                    }`}
-                    height="100"
-                    width="100"
-                    src={img?.url}
-                    alt={img?.url}
-                    onClick={() => setActiveImg(img.url)}
-                  />
-                </a>
-              </div>
-            ))}
-          </div>
-        </div>
 
-        <div className="col-12 col-lg-5 mt-5">
-          <h3>{product?.name}</h3>
-          <p id="product_id">Product # {product?._id}</p>
+          <div className="col-12 col-lg-5 mt-5">
+            <h3>{product?.name}</h3>
+            <p id="product_id">Product # {product?._id}</p>
 
-          <hr />
+            <hr />
 
-          <div className="d-flex">
-            <StarRatings
-              rating={product?.ratings}
-              starRatedColor="orange"
-              numberOfStars={5}
-              name="rating"
-              starDimension="20px"
-              starSpacing="1px"
-            />
-            <span id="no-of-reviews" className="pt-1 ps-2">
-              {" "}
-              ({product?.numOfReviews} Reviews){" "}
-            </span>
-          </div>
-          <hr />
+            <div className="d-flex">
+              <StarRatings
+                rating={product?.ratings}
+                starRatedColor="orange"
+                numberOfStars={5}
+                name="rating"
+                starDimension="20px"
+                starSpacing="1px"
+              />
+              <span id="no-of-reviews" className="pt-1 ps-2">
+                {" "}
+                ({product?.numOfReviews} Reviews){" "}
+              </span>
+            </div>
+            <hr />
 
-          <p id="product_price">${product?.price}</p>
-          <div className="stockCounter d-inline">
-            <span
-              className="btn btn-danger minus"
-              onClick={() => quantity > 0 && setQuantity((e) => Number(e) - 1)}
+            <p id="product_price">${product?.price}</p>
+            <div className="stockCounter d-inline">
+              <span
+                className="btn btn-danger minus"
+                onClick={() =>
+                  quantity > 0 && setQuantity((e) => Number(e) - 1)
+                }
+              >
+                -
+              </span>
+              <input
+                type="number"
+                className="form-control count d-inline"
+                value={quantity}
+                readOnly
+              />
+              <span
+                className="btn btn-primary plus"
+                onClick={() =>
+                  quantity < product?.stock && setQuantity((e) => Number(e) + 1)
+                }
+              >
+                +
+              </span>
+            </div>
+            <button
+              type="button"
+              id="cart_btn"
+              className="btn btn-primary d-inline ms-4"
+              disabled={product?.stock <= 0}
+              onClick={handleAddCart}
             >
-              -
-            </span>
-            <input
-              type="number"
-              className="form-control count d-inline"
-              value={quantity}
-              readOnly
-            />
-            <span
-              className="btn btn-primary plus"
-              onClick={() =>
-                quantity < product?.stock && setQuantity((e) => Number(e) + 1)
-              }
-            >
-              +
-            </span>
-          </div>
-          <button
-            type="button"
-            id="cart_btn"
-            className="btn btn-primary d-inline ms-4"
-            disabled={product?.stock <= 0}
-            onClick={handleAddCart}
-          >
-            Add to Cart
-          </button>
+              Add to Cart
+            </button>
 
-          <hr />
+            <hr />
 
-          <p>
-            Status:{" "}
-            <span
-              id="stock_status"
-              className={product?.stock > 0 ? "greenColor" : "redColor"}
-            >
-              {product?.stock > 0 ? "In Stock" : "Out off Stock"}
-            </span>
-          </p>
+            <p>
+              Status:{" "}
+              <span
+                id="stock_status"
+                className={product?.stock > 0 ? "greenColor" : "redColor"}
+              >
+                {product?.stock > 0 ? "In Stock" : "Out off Stock"}
+              </span>
+            </p>
 
-          <hr />
+            <hr />
 
-          <h4 className="mt-2">Description:</h4>
-          <p>{product?.description}</p>
-          <hr />
-          <p id="product_seller mb-3">
-            Sold by: <strong>{product?.seller}</strong>
-          </p>
+            <h4 className="mt-2">Description:</h4>
+            <p>{product?.description}</p>
+            <hr />
+            <p id="product_seller mb-3">
+              Sold by: <strong>{product?.seller}</strong>
+            </p>
 
-          <div className="alert alert-danger my-5" type="alert">
-            Login to post your review.
+            <div className="alert alert-danger my-5" type="alert">
+              Login to post your review.
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
