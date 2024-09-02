2a5357e58b58695d63f29e3fe986df1baf1a86d8
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-09-02 15:37:17 +0430

    Create cartSlice to manage the cart

---
+++ client/src/redux/features/cartSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cartItems: localStorage.getItem("cardItems")
+    ? JSON.parse(localStorage.getItem("cardItems"))
+    : [],
+};
+const cartSlice = createSlice({
+  name: "carSlice",
+  initialState,
+  reducers: {
+    setCartItem: (state, action) => {
+      const item = action.payload;
+      const isItemExist = state.cartItems.find(
+        (e) => e.product === item.product,
+      );
+      if (isItemExist) {
+        state.cartItems = state.cartItems.map((curItem) =>
+          item.product === curItem.product ? item : curItem,
+        );
+      } else {
+        state.cartItems.push(item);
+      }
+      localStorage.setItem("cardItems", JSON.stringify(state.cartItems));
+    },
+  },
+});
+
+export default cartSlice.reducer;
+export const { setCartItem } = cartSlice.actions;
