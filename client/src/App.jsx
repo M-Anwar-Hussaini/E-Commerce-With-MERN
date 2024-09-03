6970003707e64555a4edf2851854d0fd87eeb4ae
hafiz1379 <hafizrasa1379@gmail.com>
2024-09-03 11:37:59 +0430

    Add PaymentMethod component for selecting payment method during checkout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,6 +16,7 @@ import ResetPassword from "./components/auth/ResetPassword";
 import Cart from "./components/cart/Cart";
 import Shipping from "./components/cart/Shipping";
 import ConfirmOrder from "./components/cart/ConfirmOrder";
+import PaymentMethod from "./components/cart/PaymentMethod";
 
 function App() {
   return (
@@ -78,6 +79,14 @@ function App() {
               element={
                 <ProtectedRoute>
                   <ConfirmOrder />
+                </ProtectedRoute>
+              }
+            />
+             <Route
+              path="/payment_method"
+              element={
+                <ProtectedRoute>
+                  <PaymentMethod />
                 </ProtectedRoute>
               }
             />
