5afbe53883d6bf25f3ab0c95eaec3397b2b348ea
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-09-05 19:51:32 +0430

    Add the verify propery

--- api/app.js
+++ api/app.js
@@ -19,7 +19,14 @@ process.on("uncaughtException", (err) => {
 // Intial configurations
 dotenv.config({ path: "./config/config.env" });
 const app = express();
-app.use(express.json({ limit: "10mb" }));
+app.use(
+  express.json({
+    limit: "10mb",
+    verify: (req, res, buf) => {
+      req.rawBody = buf.toString();
+    },
+  }),
+);
 app.use(cookieParser());
 
 // Defining the routes:
