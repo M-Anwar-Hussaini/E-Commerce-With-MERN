78f5bd0444fa8a49a3fc00b5c8e8cc192505678b
hafiz1379 <hafizrasa1379@gmail.com>
2024-09-07 11:33:18 +0430

    Refactor success URL in stripeCheckoutSession to include order_success query parameter

--- api/controllers/paymentController.js
+++ api/controllers/paymentController.js
@@ -13,7 +13,7 @@ export const stripeCheckoutSession = catchAsyn(async (req, res) => {
   // Create the session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
-    success_url: `${process.env.FRONTEND_URL}/orders/me`,
+    success_url: `${process.env.FRONTEND_URL}/orders/me?order_success=true`,
     cancel_url: `${process.env.FRONTEND_URL}`,
     customer_email: req?.user?.email,
     client_reference_id: req?.user?._id?.toString(),
