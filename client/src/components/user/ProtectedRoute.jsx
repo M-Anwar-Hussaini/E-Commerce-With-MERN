0a0c58893abc230a8fc88d2f6cf895c2f92c509b
hafiz1379 <hafizrasa1379@gmail.com>
2024-09-10 09:29:13 +0430

    Refactor ProtectedRoute component to conditionally render children based on user role

--- client/src/components/user/ProtectedRoute.jsx
+++ client/src/components/user/ProtectedRoute.jsx
@@ -2,13 +2,17 @@ import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import Loader from "../layouts/Loader";
 import PropTypes from "prop-types";
-const ProtectedRoute = ({ children }) => {
-  const { isAuthenticated, loading } = useSelector((state) => state.auth);
+const ProtectedRoute = ({ admin, children }) => {
+  const { isAuthenticated, user, loading } = useSelector((state) => state.auth);
 
   if (loading) return <Loader />;
 
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
+  }
+
+  if (admin && user.role !== "admin") {
+    return <Navigate to="/" replace />;
   }
 
   return children;
