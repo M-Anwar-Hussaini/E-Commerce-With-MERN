880e00177aec25ccd2b84e04371e9656980c45d4
hafiz1379 <hafizrasa1379@gmail.com>
2024-09-11 16:00:45 +0430

    Add ListProducts component for admin product listing

--- client/src/components/routes/adminRoutes.jsx
+++ client/src/components/routes/adminRoutes.jsx
@@ -1,6 +1,7 @@
 import { Route } from "react-router-dom";
 import ProtectedRoute from "../user/ProtectedRoute";
 import Dashboard from "../admin/Dashboard";
+import ListProducts from "../admin/ListProducts";
 
 const adminROutes = () => {
   return (
@@ -10,6 +11,14 @@ const adminROutes = () => {
         element={
           <ProtectedRoute admin={true}>
             <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/admin/products"
+        element={
+          <ProtectedRoute admin={true}>
+            <ListProducts />
           </ProtectedRoute>
         }
       />
