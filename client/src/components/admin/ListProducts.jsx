dc30c1202436837e44b3b1ddc5b918ab8782b478
hafiz1379 <hafizrasa1379@gmail.com>
2024-09-11 16:00:35 +0430

    Add ListProducts component for admin product listing

---
+++ client/src/components/admin/ListProducts.jsx
@@ -0,0 +1,96 @@
+import { useEffect } from "react";
+import { toast } from "react-toastify";
+import Loader from "../layouts/Loader";
+import { MDBDataTable } from "mdbreact";
+import { Link } from "react-router-dom";
+import MetaData from "../layouts/MetaData";
+import { useGetAdminProductsQuery } from "../../redux/api/productsApi";
+import AdminLayout from "../layouts/AdminLayout";
+
+const ListProducts = () => {
+  const { data, isLoading, error } = useGetAdminProductsQuery();
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error?.data?.message);
+    }
+  }, [error]);
+
+  const setProducts = () => {
+    const products = {
+      columns: [
+        {
+          label: "ID",
+          field: "id",
+          sort: "asc",
+        },
+        {
+          label: "Name",
+          field: "name",
+          sort: "asc",
+        },
+        {
+          label: "Stock",
+          field: "stock",
+          sort: "asc",
+        },
+        {
+          label: "Actions",
+          field: "actions",
+          sort: "asc",
+        },
+      ],
+      rows: [],
+    };
+
+    data?.products?.forEach((product) => {
+      products.rows.push({
+        id: product?._id,
+        name: `${product?.name?.substring(0, 20)}...`,
+        stock: product?.stock,
+        actions: (
+          <>
+            <Link
+              to={`/admin/products/${product._id}`}
+              className="btn btn-outline-primary"
+            >
+              <i className="fa fa-pencil"></i>
+            </Link>
+            <Link
+              to={`/admin/products/${product._id}/upload_images`}
+              className="btn btn-outline-success ms-2"
+            >
+              <i className="fa fa-image"></i>
+            </Link>
+            <button
+              className="btn btn-outline-danger ms-2"
+            >
+              <i className="fa fa-trash"></i>
+            </button>
+          </>
+        ),
+      });
+    });
+
+    return products;
+  };
+
+  if (isLoading) return <Loader />;
+
+  return (
+    <AdminLayout>
+      <MetaData title={"All Products"} />
+      <h1 className="my-5">{data?.products?.length} Products</h1>
+
+      <MDBDataTable
+        data={setProducts()}
+        className="px-3"
+        bordered
+        striped
+        hover
+      />
+    </AdminLayout>
+  );
+};
+
+export default ListProducts;
