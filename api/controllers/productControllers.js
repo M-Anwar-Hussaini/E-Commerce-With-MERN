6af928ea170b2f6cb522f805f4a3dcc05f5af71d
hafiz1379 <hafizrasa1379@gmail.com>
2024-09-30 13:32:46 +0430

    Refactor productControllers.js to include deleteProductImage endpoint

--- api/controllers/productControllers.js
+++ api/controllers/productControllers.js
@@ -3,7 +3,7 @@ import ErrorHandler from "../utils/errorHandler.js";
 import catchcAsync from "../middlewares/catchAsyncErrors.js";
 import APIFilters from "../utils/apiFilters.js";
 import Order from "../models/orderModel.js";
-import {upload_file} from "../utils/cloudinary.js";
+import {delete_file, upload_file} from "../utils/cloudinary.js";
 
 // Get all products => /api/v1/products
 export const getProducts = catchcAsync(async (req, res) => {
@@ -83,6 +83,29 @@ export const uploadProductImages = catchcAsync(async (req, res, next) => {
 
   product?.images?.push(...urls);
   await product?.save();
+
+  res.status(200).json({
+    product,
+  });
+});
+
+// Delete product image   =>  /api/v1/admin/products/:id/delete_image
+export const deleteProductImage = catchcAsync(async (req, res, next) => {
+  let product = await Product.findById(req?.params?.id);
+
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+
+  const isDeleted = await delete_file(req.body.imgId);
+
+  if (isDeleted) {
+    product.images = product?.images?.filter(
+      (img) => img.public_id !== req.body.imgId
+    );
+
+    await product?.save();
+  }
 
   res.status(200).json({
     product,
