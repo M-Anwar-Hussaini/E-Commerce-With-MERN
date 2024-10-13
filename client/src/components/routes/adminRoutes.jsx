ebad0454e507c26f019189058d5079053910da97
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-10-13 10:24:57 +0430

    Add the getReview admin route

--- client/src/components/routes/adminRoutes.jsx
+++ client/src/components/routes/adminRoutes.jsx
@@ -9,6 +9,7 @@ import ListOrders from '../admin/ListOrders';
 import ProcessOrder from '../admin/ProcessOrder';
 import ListUsers from '../admin/ListUsers';
 import UpdateUser from '../admin/UpdateUser';
+import ProductReviews from '../admin/ProductReviews';
 
 const adminROutes = () => {
   return (
@@ -82,6 +83,14 @@ const adminROutes = () => {
         element={
           <ProtectedRoute admin={true}>
             <UpdateUser />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/admin/reviews"
+        element={
+          <ProtectedRoute admin={true}>
+            <ProductReviews />
           </ProtectedRoute>
         }
       />
