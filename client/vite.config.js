5e6d259ccd2487ba6d0f0f0406576edaa5f674ed
Hafiz Rasa <hafizrasa1379@gmail.com>
2025-06-12 20:35:08 +0430

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://e-commerce-with-mern-5t80.onrender.com",
         changeOrigin: true,
         secure: false,
       },
