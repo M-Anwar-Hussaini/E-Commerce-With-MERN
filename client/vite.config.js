5e75a147c2780f394d7be43e7e7ae58d5f4828e3
hafiz1379 <hafizrasa1379@gmail.com>
2025-06-15 17:59:59 +0430

    Add dotenv dependency and configure environment variables in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 
-// https://vitejs.dev/config/
+// Load environment variables
+dotenv.config();
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "https://e-commerce-with-mern-back.onrender.com",
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
         secure: false,
       },
